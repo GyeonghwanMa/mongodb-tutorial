5ec38469eb8687be9208b8090e30e6900c0e90d7
kyunghwanma <ghma1213@gmail.com>
2021-09-20 21:48:45 +0900

    N+1 Problem

--- client.js
+++ client.js
@@ -1,21 +1,26 @@
 console.log('client code running.');
 const axios = require('axios');
 
+const URI = 'http://localhost:3000';
+
 const test = async () => {
     let { 
         data: {blogs}
-    } = await axios.get('http://localhost:3000/blog');
+    } = await axios.get(`${URI}/blog`);
 
     // promise.all은 promise배열 리턴
     // blogs는 단순 배열
     blogs = await Promise.all(blogs.map(async blog => {
-        const res1 = await axios.get(`http://localhost:3000/user/${blog.user}`);
-        const res2 = await axios.get(`http://localhost:3000/blog/${blog._id}/comment`);
+        const [res1, res2] = await Promise.all([axios.get(`${URI}/user/${blog.user}`), axios.get(`${URI}/blog/${blog._id}/comment`)]) ;
         blog.user = res1.data.user;
-        blog.comments = res2.data.comments;
+        blog.comments = await Promise.all(res2.data.comments.map(async comment => {
+            const { data: {user} }  = await axios.get(`${URI}/user/${comment.user}`);
+            comment.user = user
+            return comment;
+        }));
         return blog;
     }))
-    console.log(blogs[0]);
+    console.dir(blogs[0], {depth: 10});
 }
 
 test();

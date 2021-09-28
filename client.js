6e40cdd543e19a86d32eda26d8aa961d09bf53bc
kyunghwanma <ghma1213@gmail.com>
2021-09-28 21:53:05 +0900

    Nesting 성능테스트

--- client.js
+++ client.js
@@ -1,38 +1,39 @@
-console.log('client code running.');
-const axios = require('axios');
+console.log("client code running.");
+const axios = require("axios");
 
-const URI = 'http://localhost:3000';
+const URI = "http://localhost:3000";
+
+// populate 방식
+// nesting 방식
 
 const test = async () => {
-    console.time("loading time: ");
-    let { 
-        data: {blogs}
-    } = await axios.get(`${URI}/blog`);
-    // console.log(blogs[3], {depth: 10});
-    // promise.all은 promise배열 리턴
-    // blogs는 단순 배열
-    // blogs = await Promise.all(blogs.map(async blog => {
-    //     const [res1, res2] = await Promise.all([axios.get(`${URI}/user/${blog.user}`), axios.get(`${URI}/blog/${blog._id}/comment`)]) ;
-    //     blog.user = res1.data.user;
-    //     blog.comments = await Promise.all(res2.data.comments.map(async comment => {
-    //         const { data: {user} }  = await axios.get(`${URI}/user/${comment.user}`);
-    //         comment.user = user
-    //         return comment;
-    //     }));
-    //     return blog;
-    //     })
-    // );
-    console.timeEnd("loading time: ");
-}
+  console.time("loading time: ");
+  await axios.get(`${URI}/blog`);
+  // console.log(blogs[3], {depth: 10});
+  // promise.all은 promise배열 리턴
+  // blogs는 단순 배열
+  // blogs = await Promise.all(blogs.map(async blog => {
+  //     const [res1, res2] = await Promise.all([axios.get(`${URI}/user/${blog.user}`), axios.get(`${URI}/blog/${blog._id}/comment`)]) ;
+  //     blog.user = res1.data.user;
+  //     blog.comments = await Promise.all(res2.data.comments.map(async comment => {
+  //         const { data: {user} }  = await axios.get(`${URI}/user/${comment.user}`);
+  //         comment.user = user
+  //         return comment;
+  //     }));
+  //     return blog;
+  //     })
+  // );
+  console.timeEnd("loading time: ");
+};
 
 const testGroup = async () => {
-    await test();
-    await test();
-    await test();
-    await test();
-    await test();
-    await test();
-    await test();
-}
+  await test();
+  await test();
+  await test();
+  await test();
+  await test();
+  await test();
+  await test();
+};
 
 testGroup();
--- src/server.js
+++ src/server.js
@@ -1,35 +1,38 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const { userRouter, blogRouter } = require('./routes');
-const mongoose = require('mongoose');
-const {generateFakeData} = require('../faker');
+const { userRouter, blogRouter } = require("./routes");
+const mongoose = require("mongoose");
+const { generateFakeData } = require("../faker2");
 
-const MONGO_URI = 'mongodb+srv://admin:NhQ4SAxtpVC3x9vV@mongodbtutorial.rmzkk.mongodb.net/BlogService?retryWrites=true&w=majority';
+const MONGO_URI =
+  "mongodb+srv://admin:NhQ4SAxtpVC3x9vV@mongodbtutorial.rmzkk.mongodb.net/BlogService?retryWrites=true&w=majority";
 
-const server = async() => {
-    try {
-        await mongoose.connect(MONGO_URI, {
-            useNewUrlParser: true, 
-            useUnifiedTopology: true
-        });
-        
-        // mongoose.set('debug', true)
-        // let result = mongoose.connect(MONGO_URI)
-        
-        // await generateFakeData(100, 10, 300);
+const server = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
 
-        // json을 자바스크립트로 변경
-        app.use(express.json());
-        
-        // url이 user로 시작할 경우 
-        app.use('/user', userRouter);
-        app.use('/blog', blogRouter);
-        
-        app.listen(3000, () => console.log('server listening on port 3000'));
-    } catch (error) {
-        console.log(error);
-    }
-}
+    // mongoose.set('debug', true)
+    // let result = mongoose.connect(MONGO_URI)
+
+    // json을 자바스크립트로 변경
+    app.use(express.json());
+
+    // url이 user로 시작할 경우
+    app.use("/user", userRouter);
+    app.use("/blog", blogRouter);
+
+    app.listen(3000, async () => {
+      console.log("server listening on port 3000");
+      //   for (let i = 0; i < 20; i++) {
+      //     await generateFakeData(10, 1, 10);
+      //   }
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 server();
-

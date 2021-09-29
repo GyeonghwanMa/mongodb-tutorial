c01184581b7b07f99a04624677d57f161ef1fab4
kyunghwanma <ghma1213@gmail.com>
2021-09-29 22:30:36 +0900

    updateMany

--- src/routes/commentRoute.js
+++ src/routes/commentRoute.js
@@ -58,4 +58,20 @@ commentRouter.get("/", async (req, res) => {
   return res.send({ comments });
 });
 
+commentRouter.patch("/:commentId", async (req, res) => {
+  const { commentId } = req.params;
+  const { content } = req.body;
+  if (typeof content !== "string")
+    return res.status(400).send({ error: "content is required" });
+
+  const [comment] = await Promise.all([
+    Comment.findOneAndUpdate({ _id: commentId }, { content }, { new: true }),
+    Blog.updateOne(
+      { "comments._id": commentId },
+      { "comments.$.content": content }
+    ),
+  ]);
+  return res.send({ comment });
+});
+
 module.exports = { commentRouter };

45bc56524e3454e2aedcd2e21c4629a7b305c281
kyunghwanma <ghma1213@gmail.com>
2021-10-26 21:42:52 +0900

    port .env 추가

--- src/server.js
+++ src/server.js
@@ -4,9 +4,10 @@ const { userRouter, blogRouter } = require("./routes");
 const mongoose = require("mongoose");
 const { generateFakeData } = require("../faker2");
 
-const { MONGO_URI } = process.env;
+const { MONGO_URI, PORT } = process.env;
 
 if (!MONGO_URI) throw new Error("MONGO_URI is required!!!");
+if (!PORT) throw new Error("PORT is required!!!");
 
 const server = async () => {
   try {
@@ -25,8 +26,8 @@ const server = async () => {
     app.use("/user", userRouter);
     app.use("/blog", blogRouter);
 
-    app.listen(3000, async () => {
-      console.log("server listening on port 3000");
+    app.listen(PORT, async () => {
+      console.log(`server listening on port ${PORT}`);
       // await generateFakeData(10, 2, 10);
     });
   } catch (error) {
